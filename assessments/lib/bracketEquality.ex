3a99cbf7aca72f1f974b39d82bc16c945ae3099c
Gopika <gopika16aug@gmail.com>
2024-10-29 16:01:46 +0530

    Assessment 4 : Brackets Equality

--- assessments/lib/bracketEquality.ex
+++ assessments/lib/bracketEquality.ex
@@ -1,10 +1,36 @@
 defmodule BracketEquality do
-  def check_bracket_equality(inputString) do
-    inputString = to_charlist(inputString)
-    if rem(length(inputString), 2) === 0 do
-      "True"
+  @brackets %{?[ => ?], ?{ => ?}, ?( => ?)}
+  @open_brackets Map.keys(@brackets)
+  @close_brackets Map.values(@brackets)
+
+  def check_bracket_equality?(input_string) do
+    input_string = to_charlist(input_string)
+
+    if rem(length(input_string), 2) === 0 do
+      input_string
+      |> isEqual?([])
     else
-      "False"
+      false
     end
   end
+
+  defp isEqual?([], []), do: true
+
+  defp isEqual?([head | tail], stack) when head in @open_brackets do
+    # IO.inspect(tail, label: "Current Tail")
+    # IO.inspect([head | stack], label: "Current Stack")
+    isEqual?(tail, [head | stack])
+  end
+
+  defp isEqual?([head | tail], [top | stack]) when head in @close_brackets do
+    # IO.inspect(tail, label: "Next Tail")
+    # IO.inspect(stack, label: "Next Stack")
+    if @brackets[top] === head do
+      isEqual?(tail, stack)
+    else
+      false
+    end
+  end
+
+  defp isEqual?(_, _), do: false
 end
